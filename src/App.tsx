02915dc21323ce51788d4c19b60f450465794964
Sther <sthefanypequeno@gmail.com>
2023-10-20 14:18:26 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>DEIXA EU CODARRRRRRRRRRRRRRRRR</h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
