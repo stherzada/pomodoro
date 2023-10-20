1b488df47ead670d98bf4ef4fe48583d3c0c2a03
Sther <sthefanypequeno@gmail.com>
2023-10-20 15:45:57 -0300

    feat: creating component pomodoro timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { PomodoroTimer } from './components/PomodoroTimer'
 
-function App() {
+function App(): JSX.Element {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <h1>DEIXA EU CODARRRRRRRRRRRRRRRRR</h1>
+        <PomodoroTimer  defaultPomodoroTime={1500} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
---
+++ src/components/PomodoroTimer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useInterval } from '../hooks/use-interval';
+
+interface Props{
+    defaultPomodoroTime: number;
+}
+
+export function PomodoroTimer(props: Props): JSX.Element{
+    const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime);
+    console.log(props.defaultPomodoroTime)
+
+  
+    setInterval(() => {
+        setMainTime(mainTime + 1);
+    }, 1000);
+
+
+    console.log(mainTime)
+    return <>
+    <div>
+        <h1>
+            {"GODDDD :("}{mainTime}
+        </h1>
+    </div>
+    
+    </>
+}
