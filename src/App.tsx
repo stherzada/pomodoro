ad476c12a11c63dde709a6ccf299c16f97df0dc4
Sther <sthefanypequeno@gmail.com>
2023-10-24 16:35:26 -0300

    fix: remove unsed imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import { PomodoroTimer } from './components/Pomodoro/PomodoroTimer'
 
--- src/components/Pomodoro/Timer.tsx
+++ src/components/Pomodoro/Timer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { secondsToTime } from '../../utils/seconds-to-time';
 import './Pomodoro.css'
 
