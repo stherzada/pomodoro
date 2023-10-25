442613ed8d91af96dd1442b503499e13ff0a2462
Sther <sthefanypequeno@gmail.com>
2023-10-25 16:49:40 -0300

    fix: time of pomodoro

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
       <div className="container">
         <h1>Pomodoro</h1>
         <PomodoroTimer
-          PomodoroTime={3600}
+          PomodoroTime={1500}
           shortRestTime={300}
           longRestTime={900}
           cycles={4}
